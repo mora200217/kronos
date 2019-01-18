00e59c63f8a62b1a1bd7c4cb4474ab381a4e5fa5
Andrés Morales <mora200217@hotmail.com>
2019-01-18 17:54:40 -0500

    React Component Basic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hola mundo!');
+import React from 'react';
+import { render } from 'react-dom';
+
+const container = document.getElementById('app');
+const holaMundo = <h1>Hola Mundo! </h1>;
+
+render(holaMundo, container);
