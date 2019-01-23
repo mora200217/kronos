ed9c4e9210dff463a9040c5e0f1503245a3236a7
Andrés Morales <mora200217@hotmail.com>
2019-01-23 13:25:48 -0500

    Clock Component Time Display

--- src/js/main/Clock.js
+++ src/js/main/Clock.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
+import React, {Component} from 'react'
+import {render} from 'react-dom'
 
-class Clock extends Component{
-  render(){
-    return(
-      <div className="col-4 time">
-        <p className="clock">12:13</p>
-        <p className="subject">Physics SL</p>
-      </div>
-    );
+class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      actualTime: new Date()
+    }
+  }
+
+  // When react-dom rendered element with clock component
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  // When react-dom deletes element with clock component
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  // Callback function, invoked every second interval
+  tick() {
+    this.setState({actualTime: new Date()});
+  }
+
+  render() {
+    return (<div className="col-4 time">
+      <p className="clock">{
+          this.state.actualTime.toLocaleTimeString(navigator.language, {
+            hour: '2-digit',
+            minute: '2-digit'
+          })
+        }</p>
+      <p className="subject">Physics SL</p>
+    </div>);
   }
 }
 
